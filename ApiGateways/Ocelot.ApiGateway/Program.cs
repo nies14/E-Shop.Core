259d7d8a3045a94a43db809176adc5ca48d448ff
Tanvir Hassan <mthnies@gmail.com>
2025-06-07 15:34:08 -0400

    added api gateway

---
+++ ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Ocelot.ApiGateway;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((env, config) =>
+            {                var environmentName = env.HostingEnvironment.EnvironmentName;
+                Console.WriteLine($"Current environment: {environmentName}");
+                // Then try the original Docker configuration
+                config.AddJsonFile("ocelot.Docker.json", optional: true, reloadOnChange: true);
+                // Then try environment-specific configuration
+                config.AddJsonFile($"ocelot.{environmentName}.json", optional: true, reloadOnChange: true);
+            }).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            }).UseSerilog(EShop.Logging.Logging.ConfigureLogger);
+}
---
+++ ApiGateways/Ocelot.ApiGateway/Startup.cs
@@ -0,0 +1,39 @@
+﻿using EShop.Logging.Correlation;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Ocelot.ApiGateway;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddScoped<ICorrelationIdGenerator, CorrelationIdGenerator>();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                policy => { policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin(); });
+        });
+
+        services.AddOcelot()
+            .AddCacheManager(o => o.WithDictionaryHandle());
+    }
+
+    public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.AddCorrelationIdMiddleware();
+        app.UseRouting();
+        app.UseCors("CorsPolicy");
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello Ocelot"); });
+        });
+        await app.UseOcelot();
+    }
+}
